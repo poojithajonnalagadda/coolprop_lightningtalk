808ba4fc1c0f002a1fb1d764c69edc0e46f8f356
poojithajonnalagadda <poojitha.jonnalagadda@utexas.edu>
2025-10-19 16:38:24 -0500

    Add files via upload

---
+++ coolprop.py
@@ -0,0 +1,19 @@
+#Using SIProps
+import CoolProp.CoolProp as CP
+
+density = CP.PropsSI('D', 'T', 298.15, 'P', 101325, 'Water')
+print(f"Density: {density:.2f} kg/m³")
+
+##
+
+state = CP.AbstractState('HEOS', 'Water')
+state.update(CP.PT_INPUTS, 101325, 300)  # P=101325 Pa, T=300 K
+
+h = state.hmass()    # Get enthalpy
+s = state.smass()    # Get entropy  
+rho = state.rhomass() # Get density
+
+print(f"Enthalpy: {h/1000:.2f} kJ/kg")
+print(f"Entropy: {s:.4f} J/kg·K") 
+print(f"Density: {rho:.2f} kg/m³")
+
