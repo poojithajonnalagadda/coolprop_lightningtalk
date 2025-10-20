4b5a9563d91ae0224213eef711cb660206b6fefd
poojithajonnalagadda <poojitha.jonnalagadda@utexas.edu>
2025-10-19 20:52:41 -0500

    Update coolprop.py

--- coolprop.py
+++ coolprop.py
@@ -1,10 +1,10 @@
-#Using SIProps
+#Example 1
 import CoolProp.CoolProp as CP
 
 density = CP.PropsSI('D', 'T', 298.15, 'P', 101325, 'Water')
 print(f"Density: {density:.2f} kg/m³")
 
-##
+#Example 2
 
 state = CP.AbstractState('HEOS', 'Water')
 state.update(CP.PT_INPUTS, 101325, 300)  # P=101325 Pa, T=300 K
@@ -16,4 +16,14 @@ rho = state.rhomass() # Get density
 print(f"Enthalpy: {h/1000:.2f} kJ/kg")
 print(f"Entropy: {s:.4f} J/kg·K") 
 print(f"Density: {rho:.2f} kg/m³")
+
+#Example 3
+from CoolProp.Plots import PropertyPlot
+plot = PropertyPlot('Water', 'Ts', unit_system='SI')
+plot.calc_isolines()
+plot.show()
+
+
+
+
 
